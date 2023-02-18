523a362f6f8baf733bf401006fa473c7be2e340d
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-02-18 22:05:04 +0700

    delete

--- admin/deleteDoctor.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("function.php");
-$id = $_GET["id"];
-if (delete_doctor($id) > 0) {
-    echo "<script>document.location.href = 'doctor.php?success=Data success deleted!';</script>";
-} else {
-    echo "<script>document.location.href = 'doctor.php?failed=Data failed deleted!';</script>";
-}
-?>
--- admin/deletePatient.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("function.php");
-$id = $_GET["id"];
-if (delete_patient($id) > 0) {
-    echo "<script>document.location.href = 'patient.php?success=Data success deleted!';</script>";
-} else {
-    echo "<script>document.location.href = 'patient.php?failed=Data failed deleted!';</script>";
-}
-?>
