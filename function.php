e17cdd208e8192bf7caee720dd42c982baf1bcda
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-02-19 00:19:00 +0700

    first commit

---
+++ function.php
@@ -0,0 +1,30 @@
+<?php
+//set default timezone
+date_default_timezone_set('Asia/Jakarta');
+// create connection
+$db = mysqli_connect('localhost', 'root', '', 'e-cure');
+if(mysqli_connect_error()) {
+    echo mysqli_connect_error();
+}
+// function base url
+function base_url($url = null) {
+    $base_url = "http://localhost/e-cure";
+    if($url != null) {
+        return $base_url."/".$url;
+    } else {
+        return $base_url;
+    }
+}
+
+// SQL Start
+function query($query) {
+    global $db;
+    $result = mysqli_query($db, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+// SQL End
+?>
---
+++ index.php
@@ -0,0 +1,122 @@
+<!-- PHP -->
+<?php
+require 'function.php';
+if (isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $result = mysqli_query($db, "SELECT * FROM tbl_user WHERE username = '$username'");
+    if (mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row["password"])) {
+            header("Location: admin/index.php");
+            exit;
+        }
+    }
+    $error = true;
+}
+?>
+<!-- PHP -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>E-CURE | Admin</title>
+    <link href="<?= base_url() ?>/assets/img/favicon.png" rel="icon">
+    <link href="<?= base_url() ?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <!-- Style CSS -->
+    <link href="<?= base_url() ?>/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Main Start -->
+    <main>
+        <div class="container">
+            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                            <!-- Logo Start -->
+                            <div class="d-flex justify-content-center py-4">
+                                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                                    <img src="assets/img/logo.png" alt="">
+                                    <span class="d-none d-lg-block">E-CURE</span>
+                                </a>
+                            </div>
+                            <!-- Logo End -->
+                            <div class="card mb-3">
+                                <div class="card-body">
+                                    <div class="pt-4 pb-2">
+                                        <h5 class="card-title text-center pb-0 fs-4">Login</h5>
+                                        <p class="text-center small">Enter your username & password to login</p>
+                                    </div>
+                                    <!-- Message Failed Start -->
+                                    <?php if(isset($error)) : ?>
+                                    <p style="color: red; font-style: italic;">Username / password wrong</p>
+                                    <?php endif; ?>
+                                    <!-- Message Failed End -->
+                                    <!-- Form Start -->
+                                    <form action="" method="POST" class="row g-3 needs-validation">
+                                        <div class="col-12">
+                                            <label for="username" class="form-label">Username</label>
+                                            <input type="text" name="username" class="form-control" id="username"
+                                                required autofocus>
+                                        </div>
+                                        <div class="col-12">
+                                            <label for="password" class="form-label">Password</label>
+                                            <input type="password" name="password" class="form-control" id="password"
+                                                required>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember"
+                                                    value="true" id="rememberMe">
+                                                <label class="form-check-label" for="rememberMe">Remember me</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100" type="submit"
+                                                name="login">Login</button>
+                                        </div>
+                                        <div class="col-12">
+                                            <p class="small mb-0">Don't have account? <a
+                                                    href="pages-register.html">Create an account</a></p>
+                                        </div>
+                                    </form>
+                                    <!-- Form End -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+    <!-- Main End -->
+    <!-- JS Start -->
+    <script src="<?= base_url() ?>/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="<?= base_url() ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>/assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="<?= base_url() ?>/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="<?= base_url() ?>/assets/vendor/quill/quill.min.js"></script>
+    <script src="<?= base_url() ?>/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="<?= base_url() ?>/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="<?= base_url() ?>/assets/vendor/php-email-form/validate.js"></script>
+    <script src="<?= base_url() ?>/assets/js/main.js"></script>
+    <!-- JS End -->
+</body>
+
+</html>
