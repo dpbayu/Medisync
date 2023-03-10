bc9913b8f9acd500d8506d7a5b1e6804d4010b19
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-03-11 02:06:51 +0700

    initial commit

--- doctor/deleteMedicalRecord.php
+++ doctor/deleteMedicalRecord.php
@@ -3,10 +3,10 @@ require '../function.php';
 $id = $_GET['id'];
 $sql = "DELETE FROM tbl_medical_record WHERE id_hospital = '$id'";
 if (mysqli_query($db, $sql)) {
-    header("Location: data.php?success=Data success deleted");
+    header("Location: dataMedicalRecord.php?success=Data success deleted");
     exit();
 } else {
-    header("Location: data.php?failed=Data failed delete");
+    header("Location: dataMedicalRecord.php?failed=Data failed delete");
     exit();    
 }
 ?>
