1f56602c9892c1823ed97497dafe6b0b6733c566
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-10-28 07:20:33 +0700

    profile patient done

--- patient/function.php
+++ patient/function.php
@@ -7,6 +7,14 @@ if (isset($_POST['update'])) {
     global $db;
     $name_patient = mysqli_real_escape_string($db, $_POST['name_patient']);
     $email_patient = mysqli_real_escape_string($db, $_POST['email_patient']);
+    $gender_patient = mysqli_real_escape_string($db, $_POST['gender_patient']);
+    $address_patient = mysqli_real_escape_string($db, $_POST['address_patient']);
+    $phone_patient = mysqli_real_escape_string($db, $_POST['phone_patient']);
+    $birth_date = mysqli_real_escape_string($db, $_POST['birth_date']);
+    $birth_place = mysqli_real_escape_string($db, $_POST['birth_place']);
+    $blood_patient = mysqli_real_escape_string($db, $_POST['blood_patient']);
+    $religion_patient = mysqli_real_escape_string($db, $_POST['religion_patient']);
+    $marriage_patient = mysqli_real_escape_string($db, $_POST['marriage_patient']);
     $password_patient = mysqli_real_escape_string($db, $_POST['password_patient']);
     $profile_patient = $_FILES['profile_patient']['name'];
     $imgtemp = $_FILES['profile_patient']['tmp_name'];
@@ -23,10 +31,18 @@ if (isset($_POST['update'])) {
     } else {
             if (empty($password_patient)) {
                 $id = $_SESSION['id_patient'];
-                $sql = "UPDATE tbl_patient SET name_patient = '$name_patient', email_patient = '$email_patient', profile_patient = '$profile_patient' WHERE id_patient = '$id'";
+                $sql = "UPDATE tbl_patient SET name_patient = '$name_patient', email_patient = '$email_patient', gender_patient = '$gender_patient', address_patient = '$address_patient', phone_patient = '$phone_patient', birth_date = '$birth_date', birth_place = '$birth_place', blood_patient = '$blood_patient', religion_patient = '$religion_patient', marriage_patient = '$marriage_patient', profile_patient = '$profile_patient' WHERE id_patient = '$id'";
                 if (mysqli_query($db, $sql)) {
                     $_SESSION['name_patient'] = $name_patient;
                     $_SESSION['email_patient'] = $email_patient;
+                    $_SESSION['gender_patient'] = $gender_patient;
+                    $_SESSION['address_patient'] = $address_patient;
+                    $_SESSION['phone_patient'] = $phone_patient;
+                    $_SESSION['birth_date'] = $birth_date;
+                    $_SESSION['birth_place'] = $birth_place;
+                    $_SESSION['blood_patient'] = $blood_patient;
+                    $_SESSION['religion_patient'] = $religion_patient;
+                    $_SESSION['marriage_patient'] = $marriage_patient;
                     $_SESSION['profile_patient'] = $profile_patient;
                     echo "<script>document.location.href = 'profile.php?success=Succesfully updated!';</script>";
                 } else {
@@ -35,7 +51,7 @@ if (isset($_POST['update'])) {
             } else {
                 $hash = password_hash($password_patient, PASSWORD_DEFAULT);
                 $id = $_SESSION['id_patient'];
-                $sql2 = "UPDATE tbl_patient SET name_patient = '$name_patient', email_patient = '$email_patient', password_patient = '$hash' WHERE id_patient = '$id'";
+                $sql2 = "UPDATE tbl_patient SET name_patient = '$name_patient', email_patient = '$email_patient', gender_patient = '$gender_patient', address_patient = '$address_patient', phone_patient = '$phone_patient', birth_date = '$birth_date', birth_place = '$birth_place', blood_patient = '$blood_patient', religion_patient = '$religion_patient', marriage_patient = '$marriage_patient', profile_patient = '$profile_patient', password_patient = '$hash' WHERE id_patient = '$id'";
                 if (mysqli_query($db, $sql2)) {
                     session_unset();
                     session_destroy();
--- patient/profile.php
+++ patient/profile.php
@@ -36,32 +36,100 @@ $page = 'profile';
             <div class="col-md-12">
                 <div class="row">
                     <?php
-                        if (isset($_GET['success'])) {
-                            $msg = $_GET['success'];
-                            echo '
-                            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                            <strong>'.$msg.'</strong>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            </div>';
-                        }
+                    if (isset($_GET['success'])) {
+                        $msg = $_GET['success'];
+                        echo '
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>' . $msg . '</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>';
+                    }
                     ?>
                     <div class="col-md-6">
                         <form class="forms-sample" action="function.php" method="POST" enctype="multipart/form-data">
-                            <img src="img/<?php echo $_SESSION['profile_patient'] ?>" class="rounded-circle"
-                                height="200" width="200">
+                            <img src="img/<?php echo $_SESSION['profile_patient'] ?>" class="rounded-circle" height="200" width="200">
                             <div class="form-group mb-3">
                                 <label class="form-label" for="profil">Profile</label>
                                 <input class="form-control" type="file" id="profile" name="profile_patient">
                             </div>
                             <div class="form-group mb-3">
                                 <label class="form-label" for="name_patient">Fullname</label>
-                                <input class="form-control" type="text" id="name_patient" name="name_patient"
-                                    value="<?php echo $_SESSION['name_patient'] ?>">
+                                <input class="form-control" type="text" id="name_patient" name="name_patient" value="<?php echo $_SESSION['name_patient'] ?>">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="form-label" for="gender_patient">Gender</label>
+                                <div class="d-flex gap-3">
+                                    <label class="form-label" class="radio-inline">
+                                        <input type="radio" name="gender_patient" value="Man" <?php echo $_SESSION['gender_patient'] == "Man" ? "checked" : null ?> required> Man
+                                    </label>
+                                    <label class="form-label" class="radio-inline">
+                                        <input type="radio" name="gender_patient" value="Woman" <?php echo $_SESSION['gender_patient'] == "Woman" ? "checked" : null ?> required> Woman
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group mb-3 col">
+                                <label class="form-label" for="BloodType">Blood Type</label>
+                                <select class="form-control" id="BloodType" name="blood_patient">
+                                    <option value="A" <?php echo $_SESSION['blood_patient'] == "A" ? "selected" : '' ?>>A
+                                    </option>
+                                    <option value="B" <?php echo $_SESSION['blood_patient'] == "B" ? "selected" : '' ?>>B
+                                    </option>
+                                    <option value="AB" <?php echo $_SESSION['blood_patient'] == "AB" ? "selected" : '' ?>>AB
+                                    </option>
+                                    <option value="O" <?php echo $_SESSION['blood_patient'] == "O" ? "selected" : '' ?>>O</option>
+                                </select>
+                            </div>
+                            <div class="d-flex gap-5">
+                                <div class="form-group mb-3 col">
+                                    <label class="form-label" for="birth_place">Birth Place</label>
+                                    <input class="form-control" type="text" id="birth_place" name="birth_place" value="<?php echo $_SESSION['birth_place'] ?>" required>
+                                </div>
+                                <div class="form-group mb-3 col">
+                                    <label class="form-label" for="birth_date">Birth Date Patient</label>
+                                    <input class="form-control" type="date" id="birth_date" name="birth_date" value="<?= date('Y-m-d', strtotime($_SESSION["birth_date"])) ?>">
+                                </div>
                             </div>
                             <div class="form-group mb-3">
                                 <label class="form-label" for="email">Email</label>
-                                <input class="form-control" type="text" id="email" name="email_patient"
-                                    value="<?php echo $_SESSION['email_patient'] ?>">
+                                <input class="form-control" type="email" id="email" name="email_patient" value="<?php echo $_SESSION['email_patient'] ?>">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="form-label" for="address">Address</label>
+                                <input class="form-control" type="text" id="address" name="address_patient" value="<?php echo $_SESSION['address_patient'] ?>">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="form-label" for="phone">Phone</label>
+                                <input class="form-control" type="number" id="phone" name="phone_patient" value="<?php echo $_SESSION['phone_patient'] ?>">
+                            </div>
+                            <div class="d-flex gap-5">
+                                <div class="form-group mb-3 col">
+                                    <label class="form-label" for="Religion">Religion</label>
+                                    <select class="form-control" id="Religion" name="religion_patient">
+                                        <option value="Islam" <?php echo $_SESSION['religion_patient'] == "Islam" ? "selected" : '' ?>>
+                                            Islam
+                                        </option>
+                                        <option value="Christian" <?php echo $_SESSION['religion_patient'] == "Christian" ? "selected" : '' ?>>Christian
+                                        </option>
+                                        <option value="Chatolic" <?php echo $_SESSION['religion_patient'] == "Chatolic" ? "selected" : '' ?>>Chatolic
+                                        </option>
+                                        <option value="Hindu" <?php echo $_SESSION['religion_patient'] == "Hindu" ? "selected" : '' ?>>
+                                            Hindu</option>
+                                        <option value="Budha" <?php echo $_SESSION['religion_patient'] == "Budha" ? "selected" : '' ?>>
+                                            Budha</option>
+                                    </select>
+                                </div>
+                                <div class="form-group mb-3 col">
+                                    <label class="form-label" for="MariaggePatient">Marital Status</label>
+                                    <select class="form-control" id="MariaggePatient" name="marriage_patient">
+                                        <option value="Married" <?php echo $_SESSION['marriage_patient'] == "Married" ? "selected" : '' ?>>Married
+                                        </option>
+                                        <option value="Not Married" <?php echo $_SESSION['marriage_patient'] == "Not Married" ? "selected" : '' ?>>Not Married
+                                        </option>
+                                        <option value="Widow" <?php echo $_SESSION['marriage_patient'] == "Widow" ? "selected" : '' ?>>
+                                            Widow</option>
+                                        <option value="Widower" <?php echo $_SESSION['marriage_patient'] == "Widower" ? "selected" : '' ?>>Widower</option>
+                                    </select>
+                                </div>
                             </div>
                             <div class="form-group mb-3">
                                 <label class="form-label" for="password">Password</label>
@@ -77,8 +145,7 @@ $page = 'profile';
                             </tr>
                             <tr>
                                 <td class="w-50">Profile</td>
-                                <td class="text-center"><img src="img/<?php echo $_SESSION['profile_patient'] ?>" class="rounded-circle"
-                                        alt="profile patient" width="100" height="100">
+                                <td class="text-center"><img src="img/<?php echo $_SESSION['profile_patient'] ?>" class="rounded-circle" alt="profile patient" width="100" height="100">
                                 </td>
                             </tr>
                             <tr>
