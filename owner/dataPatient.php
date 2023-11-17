4ad4754183d3646d4eec655bea78cff43dcda0fd
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-11-17 14:07:03 +0700

    inital commit

--- owner/dataPatient.php
+++ owner/dataPatient.php
@@ -36,7 +36,7 @@ $page = 'patient';
                         $msg = $_GET['success'];
                         echo '
                         <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <strong>'.$msg.'</strong>
+                        <strong>' . $msg . '</strong>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>';
                     }
@@ -44,7 +44,7 @@ $page = 'patient';
                         $msg = $_GET['failed'];
                         echo '
                         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>'.$msg.'</strong>
+                        <strong>' . $msg . '</strong>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>';
                     }
@@ -64,104 +64,106 @@ $page = 'patient';
                             </thead>
                             <tbody>
                                 <?php
-                                    $id = @$_GET['id_patient'];
-                                    $query_patient = "SELECT * FROM tbl_patient ORDER BY name_patient ASC";
-                                    $run_patient = mysqli_query($db,$query_patient);
-                                    $i = 1;
-                                    while ($patient = mysqli_fetch_array($run_patient)) {
+                                $id = @$_GET['id_patient'];
+                                $query_patient = "SELECT * FROM tbl_patient ORDER BY name_patient ASC";
+                                $run_patient = mysqli_query($db, $query_patient);
+                                $i = 1;
+                                while ($patient = mysqli_fetch_array($run_patient)) {
                                 ?>
-                                <?php
+                                    <?php
                                     $birth_date = new DateTime($patient['birth_date']);
                                     $selisih = date_diff($birth_date, new DateTime());
                                     $year = $selisih->y;
                                     $month = $selisih->m;
-                                ?>
-                                <tr>
-                                    <td><?= $i ?></td>
-                                    <td><?= $patient['nik_patient'] ?></td>
-                                    <td><?= $patient['name_patient'] ?></td>
-                                    <td><?= $patient['gender_patient'] ?></td>
-                                    <td><?= $patient['address_patient'] ?></td>
-                                    <td><?= $year. " years " .$month. " month " ?></td>
-                                    <td class="text-center">
-                                        <button type="button" class="btn btn-info" data-bs-toggle="modal"
-                                            data-bs-target="#modal<?= $patient['id_patient'] ?>">
-                                            View
-                                        </button>
-                                    </td>
-                                </tr>
-                                <!-- Modal Start -->
-                                <div class="modal fade" id="modal<?= $patient['id_patient'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel"
-                                    aria-hidden="true">
-                                    <div class="modal-dialog bg-white">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Patient <span
-                                                        class="fw-bold"><?= $patient['name_patient'] ?></span></h1>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="text-center mb-3">
-                                                    <img src="../patient/img/<?= $patient['profile_patient'] ?>" width="200" height="200" class="rounded-circle" alt="Image Patient">
+                                    ?>
+                                    <tr>
+                                        <td><?= $i ?></td>
+                                        <td><?= $patient['nik_patient'] ?></td>
+                                        <td><?= $patient['name_patient'] ?></td>
+                                        <td><?= $patient['gender_patient'] ?></td>
+                                        <td><?= $patient['address_patient'] ?></td>
+                                        <td><?= $year . " years " . $month . " month " ?></td>
+                                        <td class="text-center">
+                                            <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#modal<?= $patient['id_patient'] ?>">
+                                                View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <!-- Modal Start -->
+                                    <div class="modal fade" id="modal<?= $patient['id_patient'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog bg-white">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Patient <span class="fw-bold"><?= $patient['name_patient'] ?></span></h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">NIK</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['nik_patient'] ?></p>
+                                                <div class="modal-body">
+                                                    <div class="text-center mb-3">
+                                                        <?php
+                                                        if (empty($patient['profile_patient'])) {
+                                                            echo "<img width='200' height='200' src='../assets/img/User.png' />";
+                                                        } else {
+                                                            echo "<img src='../patient/img/" . $patient['profile_patient'] . "'width='200' height='200' class='rounded-circle'
+                                                            alt='Image Patient'>";
+                                                        }
+                                                        ?>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">NIK</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['nik_patient'] ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Name</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['name_patient'] ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Place, date birth</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['birth_place'] ?>,
+                                                            <?= date("j F Y", strtotime($patient['birth_date'])) ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Gender</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['gender_patient'] ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Blood type</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['blood_patient'] ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Phone</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['phone_patient'] ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Address</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['address_patient'] ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Religion</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['religion_patient'] ?></p>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <label style="width: 150px;">Marital Status</label>
+                                                        <p class="mx-3">:</p>
+                                                        <p><?= $patient['marriage_patient'] ?></p>
+                                                    </div>
                                                 </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Name</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['name_patient'] ?></p>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                                 </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Place, date birth</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['birth_place'] ?>,
-                                                        <?= date("j F Y", strtotime($patient['birth_date'])) ?></p>
-                                                </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Gender</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['gender_patient'] ?></p>
-                                                </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Blood type</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['blood_patient'] ?></p>
-                                                </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Phone</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['phone_patient'] ?></p>
-                                                </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Address</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['address_patient'] ?></p>
-                                                </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Religion</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['religion_patient'] ?></p>
-                                                </div>
-                                                <div class="d-flex">
-                                                    <label style="width: 150px;">Marital Status</label>
-                                                    <p class="mx-3">:</p>
-                                                    <p><?= $patient['marriage_patient'] ?></p>
-                                                </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                    data-bs-dismiss="modal">Close</button>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <!-- Modal End -->
+                                    <!-- Modal End -->
                                 <?php
-                                $i++; 
+                                    $i++;
                                 }
                                 ?>
                             </tbody>
@@ -175,7 +177,7 @@ $page = 'patient';
     <!-- JS Start -->
     <script>
         // Data Tables
-        $(document).ready(function () {
+        $(document).ready(function() {
             $('#patient').DataTable({
                 columnDefs: [{
                     "searchable": false,
