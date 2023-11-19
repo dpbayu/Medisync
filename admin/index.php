7591e5b65278c49a44e098e0c47af508d09fa238
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-11-19 13:58:25 +0700

    fix dashboard admin & sidebar admin

--- admin/index.php
+++ admin/index.php
@@ -36,35 +36,14 @@ $page = 'dashboard';
                             <div class="card">
                                 <div class="card-body">
                                     <div class="d-flex gap-4 py-4 px-3">
-                                        <i class="bi bi-diagram-2 rounded-circle fs-1 py-1 px-3 text-success" style="background-color: rgba(99, 255, 86, 0.2);"></i></i>
-                                        <div class="d-block">
-                                            <h2 class="fw-bolder">
-                                                <?php
-                                                    $sql = "SELECT * FROM tbl_doctor";
-                                                    $query = mysqli_query($db, $sql);
-                                                    $count = mysqli_num_rows($query);
-                                                    echo "$count";
-                                                ?>
-                                            </h2>
-                                            <h6 class="text-secondary">Doctors</h6>
-                                        </div>
-                                    </div>
-                                    <a href="../admin/dataDoctor.php" class="btn btn-primary w-100">Doctors</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="d-flex gap-4 py-4 px-3">
                                         <i class="bi bi-people rounded-circle fs-1 py-1 px-3 text-danger" style="background-color: rgba(255, 99, 132, 0.2);"></i></i>
                                         <div class="d-block">
                                             <h2 class="fw-bolder">
                                                 <?php
-                                                    $sql = "SELECT * FROM tbl_patient";
-                                                    $query = mysqli_query($db, $sql);
-                                                    $count = mysqli_num_rows($query);
-                                                    echo "$count";
+                                                $sql = "SELECT * FROM tbl_patient";
+                                                $query = mysqli_query($db, $sql);
+                                                $count = mysqli_num_rows($query);
+                                                echo "$count";
                                                 ?>
                                             </h2>
                                             <h6 class="text-secondary">Patients</h6>
@@ -78,14 +57,35 @@ $page = 'dashboard';
                             <div class="card">
                                 <div class="card-body">
                                     <div class="d-flex gap-4 py-4 px-3">
-                                        <i class="bi bi-capsule rounded-circle fs-1 py-1 px-3 text-warning" style="background-color: rgba(255, 205, 86, 0.2);"></i></i>
+                                        <i class="fa fa-user-md rounded-circle fs-1 py-3 px-4 text-success" style="background-color: rgba(99, 255, 86, 0.2);"></i></i>
                                         <div class="d-block">
                                             <h2 class="fw-bolder">
                                                 <?php
-                                                    $sql = "SELECT * FROM tbl_medicine";
-                                                    $query = mysqli_query($db, $sql);
-                                                    $count = mysqli_num_rows($query);
-                                                    echo "$count";
+                                                $sql = "SELECT * FROM tbl_doctor";
+                                                $query = mysqli_query($db, $sql);
+                                                $count = mysqli_num_rows($query);
+                                                echo "$count";
+                                                ?>
+                                            </h2>
+                                            <h6 class="text-secondary">Doctors</h6>
+                                        </div>
+                                    </div>
+                                    <a href="../admin/dataDoctor.php" class="btn btn-primary w-100">Doctors</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex gap-4 py-4 px-3">
+                                        <i class="fa fa-medkit rounded-circle fs-1 py-3 px-3 text-warning" style="background-color: rgba(255, 205, 86, 0.2);"></i></i>
+                                        <div class="d-block">
+                                            <h2 class="fw-bolder">
+                                                <?php
+                                                $sql = "SELECT * FROM tbl_medicine";
+                                                $query = mysqli_query($db, $sql);
+                                                $count = mysqli_num_rows($query);
+                                                echo "$count";
                                                 ?>
                                             </h2>
                                             <h6 class="text-secondary">Medicines</h6>
@@ -103,10 +103,10 @@ $page = 'dashboard';
                                         <div class="d-block">
                                             <h2 class="fw-bolder">
                                                 <?php
-                                                    $sql = "SELECT * FROM tbl_poly";
-                                                    $query = mysqli_query($db, $sql);
-                                                    $count = mysqli_num_rows($query);
-                                                    echo "$count";
+                                                $sql = "SELECT * FROM tbl_poly";
+                                                $query = mysqli_query($db, $sql);
+                                                $count = mysqli_num_rows($query);
+                                                echo "$count";
                                                 ?>
                                             </h2>
                                             <h6 class="text-secondary">Polys</h6>
@@ -116,7 +116,72 @@ $page = 'dashboard';
                                 </div>
                             </div>
                         </div>
-                    </div>                
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex gap-4 py-4 px-3">
+                                        <i class="fa fa-hospital-o rounded-circle fs-1 py-3 px-4 text-dark" style="background-color: rgba(200, 200, 250, 0.2);"></i></i>
+                                        <div class="d-block">
+                                            <h2 class="fw-bolder">
+                                                <?php
+                                                $sql = "SELECT * FROM tbl_specialist";
+                                                $query = mysqli_query($db, $sql);
+                                                $count = mysqli_num_rows($query);
+                                                echo "$count";
+                                                ?>
+                                            </h2>
+                                            <h6 class="text-secondary">Specialist</h6>
+                                        </div>
+                                    </div>
+                                    <a href="../admin/dataSpecialist.php" class="btn btn-primary w-100">Specialist</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex gap-4 py-4 px-3">
+                                        <i class="bi bi-diagram-2 rounded-circle fs-1 py-1 px-3 text-white" style="background-color: rgba(108, 117, 125, 0.2);"></i></i>
+                                        <div class="d-block">
+                                            <h2 class="fw-bolder">
+                                                <?php
+                                                $sql = "SELECT * FROM tbl_medical_record";
+                                                $query = mysqli_query($db, $sql);
+                                                $count = mysqli_num_rows($query);
+                                                echo "$count";
+                                                ?>
+                                            </h2>
+                                            <h6 class="text-secondary">Medical Records</h6>
+                                        </div>
+                                    </div>
+                                    <a href="../admin/dataMedicalRecord.php" class="btn btn-primary w-100">Medical Records</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex gap-4 py-4 px-3">
+                                        <i class="bi bi-bag rounded-circle fs-1 py-1 px-3 text-info" style="background-color: rgba(13, 202, 240, 0.2);"></i></i>
+                                        <div class="d-block">
+                                            <h2 class="fw-bolder">
+                                                <?php
+                                                $sql = "SELECT * FROM tbl_pharmacist";
+                                                $query = mysqli_query($db, $sql);
+                                                $count = mysqli_num_rows($query);
+                                                echo "$count";
+                                                ?>
+                                            </h2>
+                                            <h6 class="text-secondary">Pharmacist</h6>
+                                        </div>
+                                    </div>
+                                    <a href="../admin/dataPharmacist.php" class="btn btn-primary w-100">Pharmacist</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- Left Side Start -->
                 <div class="col-lg-8">
@@ -126,54 +191,64 @@ $page = 'dashboard';
                             <div class="card">
                                 <div class="card-body">
                                     <h5 class="card-title">Chart Medisync</h5>
-                                    <canvas id="barChart"
-                                        style="max-height: 900; display: block; box-sizing: border-box; height: 244px; width: 488px;"
-                                        width="976" height="488"></canvas>
+                                    <canvas id="barChart" style="max-height: 900; display: block; box-sizing: border-box; height: 244px; width: 488px;" width="976" height="488"></canvas>
                                     <script>
                                         document.addEventListener("DOMContentLoaded", () => {
                                             new Chart(document.querySelector('#barChart'), {
                                                 type: 'bar',
                                                 data: {
-                                                    labels: ['Doctor', 'Patient', 'Medicine', 'Poly',
-                                                        'Specialist'
+                                                    labels: ['Patient', 'Doctor', 'Medicine', 'Poly',
+                                                        'Specialist', 'Medical Record', 'Pharmacist'
                                                     ],
                                                     datasets: [{
                                                         label: 'Medisync',
                                                         data: [
-                                                            <?php 
-                                                            $patients = mysqli_query($db,"SELECT * FROM tbl_doctor");
+                                                            <?php
+                                                            $patients = mysqli_query($db, "SELECT * FROM tbl_patient");
                                                             echo mysqli_num_rows($patients);
                                                             ?>,
-                                                            <?php 
-                                                            $doctors = mysqli_query($db,"SELECT * FROM tbl_patient");
+                                                            <?php
+                                                            $doctors = mysqli_query($db, "SELECT * FROM tbl_doctor");
                                                             echo mysqli_num_rows($doctors);
                                                             ?>,
-                                                            <?php 
-                                                            $medicines = mysqli_query($db,"SELECT * FROM tbl_medicine");
+                                                            <?php
+                                                            $medicines = mysqli_query($db, "SELECT * FROM tbl_medicine");
                                                             echo mysqli_num_rows($medicines);
                                                             ?>,
-                                                            <?php 
-                                                            $polys = mysqli_query($db,"SELECT * FROM tbl_poly");
+                                                            <?php
+                                                            $polys = mysqli_query($db, "SELECT * FROM tbl_poly");
                                                             echo mysqli_num_rows($polys);
                                                             ?>,
-                                                            <?php 
-                                                            $specialists = mysqli_query($db,"SELECT * FROM tbl_specialist");
+                                                            <?php
+                                                            $specialists = mysqli_query($db, "SELECT * FROM tbl_specialist");
                                                             echo mysqli_num_rows($specialists);
+                                                            ?>,
+                                                            <?php
+                                                            $medicalRecords = mysqli_query($db, "SELECT * FROM tbl_medical_record");
+                                                            echo mysqli_num_rows($medicalRecords);
+                                                            ?>,
+                                                            <?php
+                                                            $pharmacists = mysqli_query($db, "SELECT * FROM tbl_pharmacist");
+                                                            echo mysqli_num_rows($pharmacists);
                                                             ?>
-                                                            ],
+                                                        ],
                                                         backgroundColor: [
-                                                            'rgba(99, 255, 86, 0.2)',
                                                             'rgba(255, 99, 132, 0.2)',
+                                                            'rgba(99, 255, 86, 0.2)',
                                                             'rgba(255, 205, 86, 0.2)',
-                                                            'rgba(75, 192, 192, 0.2)',
-                                                            'rgba(54, 162, 235, 0.2)'
+                                                            'rgba(54, 162, 235, 0.2)',
+                                                            'rgba(200, 200, 250, 0.2)',
+                                                            'rgba(108, 117, 125, 0.2)',
+                                                            'rgba(13, 202, 240, 0.2)'
                                                         ],
                                                         borderColor: [
-                                                            'rgb(99, 255, 86)',
                                                             'rgb(255, 99, 132)',
+                                                            'rgb(99, 255, 86)',
                                                             'rgb(255, 205, 86)',
-                                                            'rgb(75, 192, 192)',
-                                                            'rgb(54, 162, 235)'
+                                                            'rgb(54, 162, 235)',
+                                                            'rgb(200, 200, 250)',
+                                                            'rgb(108, 117, 125)',
+                                                            'rgb(13, 202, 240)'
                                                         ],
                                                         borderWidth: 1
                                                     }]
